8b4d0e44db0b6014fd558558df4209fbeb28f602
Alex Reznichenko <alx.reznichenko72@gmail.com>
2017-10-12 18:14:04 +0300

    some server logic in auth controller

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,5 +1,33 @@
-exports.signup = function (req, res, next) {
-    res.send({
-        sucess: true
-    })
-}
+const User = require("../models/user");
+
+exports.signup = function(req, res, next) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(402).send({
+      error: "Not valid action"
+    });
+  }
+
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    }
+    if (existingUser) {
+      res.status(422).send({
+        error: "Email is in use"
+      });
+    }
+    const user = new User({
+        email: email,
+        password: password
+    });
+    user.save( function( err ) {
+        if (err) {
+            return next(err);
+        }
+        res.json(user);
+    } );
+
+  });
+};
